00d5c339b88ff4d151bfdd589a91376fbfbdc62b
jueluddin <jueluddin@gmail.com>
2020-03-14 16:06:54 -0400

    first commit

---
+++ src/test/java/myPackage/MyStepdefs.java
@@ -0,0 +1,80 @@
+package myPackage;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyStepdefs {
+    ChromeDriver browser;
+
+    @Given("^I am on google search page$")
+    public void i_am_on_google_search_page() throws Exception {
+        // opens chrome browser
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        browser = new ChromeDriver();
+        // go to a url
+        String url = "https://www.google.com/";
+        browser.get(url);
+    }
+
+    @When("^I enter \"([^\"]*)\" into the search box$")
+    public void i_enter_into_the_search_box(String address) throws Exception {
+        browser.findElement(By.name("q")).sendKeys(address);
+        browser.findElement(By.name("q")).sendKeys(Keys.RETURN);
+    }
+
+    @Then("^I should see search result showing the same search \"([^\"]*)\" item$")
+    public void i_should_see_search_result_showing_the_same_search_item(String expectedResult) throws Exception {
+        boolean actual_result = browser.getPageSource().contains(expectedResult);
+        Assert.assertTrue(actual_result);
+    }
+
+
+    @Given("^I am on the home page of mercury tours$")
+    public void iAmOnTheHomePageOfMercuryTours() {
+        // opens chrome browser
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        browser = new ChromeDriver();
+        // go to a url
+        String url = "http://newtours.demoaut.com/";
+        browser.get(url);
+    }
+
+    @When("^I click on the register link$")
+    public void iClickOnTheRegisterLink() {
+        browser.findElement(By.linkText("REGISTER")).click();
+    }
+
+    @And("^I should see register page$")
+    public void iShouldSeeRegisterPage() {
+        String expect_page_title = "Register: Mercury Tours";
+        String actual_title = browser.getTitle();
+        Assert.assertEquals(actual_title, expect_page_title);
+    }
+
+    @And("^I enter all required information$")
+    public void iEnterAllRequiredInformation() {
+        browser.findElement(By.id("email")).sendKeys("autotester@gmail.com");
+        browser.findElement(By.name("password")).sendKeys("myPassword");
+        browser.findElement(By.name("confirmPassword")).sendKeys("myPassword");
+        // click enter after that
+        browser.findElement(By.name("register")).click();
+    }
+
+    @Then("^I should see \"([^\"]*)\"$")
+    public void iShouldSee(String expected_test) throws Throwable {
+        Assert.assertTrue(browser.getPageSource().contains(expected_test));
+    }
+
+    @Then("^I close the browser$")
+    public void iCloseTheBrowser() {
+        browser.close();
+        browser.quit();
+    }
+}
---
+++ src/test/resources/first_test.feature
@@ -0,0 +1,31 @@
+Feature: As a user I want to use google Search
+  in order to find what I need.
+
+
+  Scenario: Verify that user can search for address
+    Given I am on google search page
+    When I enter "34-18 Northern Blvd" into the search box
+    Then I should see search result showing the same search "34-18 Northern Blvd" item
+    Then I close the browser
+
+
+  Scenario Outline: Verify that user can search business with business name
+    Given I am on google search page
+    When I enter "<business_name>" into the search box
+    Then I should see search result showing the same search "<expected_result>" item
+    Then I close the browser
+
+    Examples:
+      | business_name | expected_result |
+      | apple         | apple           |
+      | chase         | chase           |
+      | amazon        | amazon          |
+
+
+  Scenario: Verify that user can register for new account -mercury tours
+    Given I am on the home page of mercury tours
+    When I click on the register link
+    And I should see register page
+    And I enter all required information
+    Then I should see "Thank you for registering"
+    Then I close the browser
